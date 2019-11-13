b37ba1dfd965bc637d0efe13c4e24347d061223e
TERANET GAME ONLINE <maenggar.map@gmail.com>
2019-11-13 14:47:54 +0700

    [Maenggar] Structuring Project Folder

---
+++ starwars_app/src/components/SignIn.js
@@ -0,0 +1,6 @@
+import React from "react"
+function SigIn(){
+    return(
+        <div>signIn page</div>
+    )
+}export default SigIn 
---
+++ starwars_app/src/components/SignOut.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignOut(){
+    return(
+        <div>
+            signout page
+        </div>
+    )
+}export default SignOut
