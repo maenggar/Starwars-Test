0c227c74c796a1527cc2be628a5b8a387368918b
TERANET GAME ONLINE <maenggar.map@gmail.com>
2019-11-17 11:31:31 +0700

    [Maenggar]detailPreview

--- starwars_app/src/components/DetailPreview.js
+++ starwars_app/src/components/DetailPreview.js
@@ -1,73 +1,27 @@
-import React from "react";
-import {
-  Card,
-  Grid,
-  Typography,
-  Divider,
-  Paper,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  CardActions,
-  Button
-} from "@material-ui/core";
+import React, { useState } from "react";
+import { Card, Grid, Typography, Divider, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useParams } from "react-router-dom";
 
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 345
-  }
-});
-
-function DetailPreview() {
-  const classes = useStyles();
+function DetailPreview(props) {
+  let films = useState(props.films);
   let { id } = useParams();
-  console.log(id);
+
+  console.log(id, "id dari params");
 
   return (
     <div>
       <Card>
-        <Typography>Detail's Preview</Typography>
-        <Divider variant="inset" component="li" />
+        <Divider variant="inset" />
         <Grid container spacing={3}>
-          <Grid item md={5}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Lizard
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Share
-                </Button>
-                <Button size="small" color="primary">
-                  Learn More
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item md={7}>
+          <Grid item md>
             <h3>{`DETAIL:${id}`}</h3>
+            <ul>
+              <li key={films[0][id - 1].episode_id}>
+                <Typography>{films[0][id - 1].title}</Typography>
+                <Typography>{films[0][id - 1].opening_crawl}</Typography>
+              </li>
+            </ul>
           </Grid>
         </Grid>
         <Grid container>
